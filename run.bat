8fdcd6a2cebc1cdb7c3c910d0d97ce1e4af7ca68
kawanishi219 <s.kawanishi291@gmail.com>
2022-05-15 20:58:55 +0900

    .bat

---
+++ run.bat
@@ -0,0 +1,27 @@
+@echo off
+cd %~dp0
+pipenv run python main.py
+
+chcp 65001
+setlocal enabledelayedexpansion
+SET INIFILE=config.ini
+SET FLAG=FALSE
+SET /a Y=%date:~0,4%
+SET /a M=%date:~5,2%
+SET /a B_M=%M%-1
+SET /a A_M=%M%+1
+
+for /f %%a in (%INIFILE%) do (
+   if %%a == [FILE] (
+      SET FLAG=TRUE
+   ) else (
+      if !FLAG! == TRUE (
+         SET file=%%a
+      )
+      SET FLAG=FALSE
+   )
+)
+
+start ./text/%B_M%-%A_M%%file:~5%%Y%.txt
+
+exit /b
