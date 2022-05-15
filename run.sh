7525a6f43c79dae78e3195b3a1e1188741c58a4f
kawanishi219 <s.kawanishi291@gmail.com>
2022-05-15 20:59:01 +0900

    .sh

--- run.sh
+++ run.sh
@@ -1,4 +1,24 @@
 #!/bin/zsh
 
+# text create
 pipenv run python main.py
+
+# ini setting
+INI_FILE=config.ini
+INI_SECTION=FILE
+
+# ini parser
+eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+    -e 's/;.*$//' \
+    -e 's/[[:space:]]*$//' \
+    -e 's/^[[:space:]]*//' \
+    -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
+   < $INI_FILE \
+    | sed -n -e "/^\[$INI_SECTION\]/,/^\s*\[/{/^[^;].*\=.*/p;}"`
+
+# text open
+month=`date '+%-m'`
+year=`date '+%-Y'`
+open ./text/$(($month-1))-$(($month+1))$FILE$year.txt
+
 exit
